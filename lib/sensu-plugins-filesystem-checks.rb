3e6827b679110b63cbcd9574fd16b2fb4ec64a98
Matt Jones <mattjones@yieldbot.com>
2015-06-03 10:53:54 -0400

    deploy

--- lib/sensu-plugins-filesystem-checks.rb
+++ lib/sensu-plugins-filesystem-checks.rb
@@ -1,14 +1,1 @@
 require 'sensu-plugins-filesystem-checks/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsFilesystemChecks
-  class << self
-  end
-
-  class << self
-  end
-end
