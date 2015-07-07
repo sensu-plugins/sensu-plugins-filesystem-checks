21700db49a25e0678a7bdcfbeaaf37b590b47225
Travis Webb <travis.w.dev@gmail.com>
2015-07-07 10:33:17 -0500

    Update check-checksums.rb

--- bin/check-checksums.rb
+++ bin/check-checksums.rb
@@ -34,38 +34,40 @@ class Checksum < Sensu::Plugin::Check::CLI
          short: '-f FILES',
          long: '--files FILES',
          required: true
-        
+  
   option :hash,
          description: 'The hash these files must hash as. If unspecified the files will be compared to the first file.',
          short: '-h SHA2HASH',
          long: '--hash SHA2HASH'
-        
+  
   option :hashfile,
          description: 'The file containing the hash these files must hash as.',
          # i.e. sha256sum filename | awk '{print $1}' > filename.sha256sum
          short: '-H SHA2HASHFILE',
          long: '--hashfile SHA2HASHFILE'
-        
+  
   option :warn_only,
          description: "Warn instead of critical if they don't match",
          short: '-w',
          long: '--warn-only',
          boolean: true
+
   def run
+
     files = config[:files].split(',')
-    
+
     if files.length == 1 && !config[:hash] && !config[:hashfile]
       unknown 'We have nothing to compare this file with.'
     end
-    
+
     if config[:hashfile]
       hash = IO.read(config[:hashfile]).chomp
     else
       hash = config[:hash] || Digest::SHA2.file(files.first).hexdigest
     end
-    
+
     errors = []
-    
+
     files.each do |file|
       if File.exist?(file)
         file_hash = Digest::SHA2.file(file).hexdigest.chomp
@@ -74,7 +76,7 @@ class Checksum < Sensu::Plugin::Check::CLI
         errors << "#{file} does not exist"
       end
     end
-    
+
     if errors.length > 0 && config[:warn_only]
       warning errors.join("\n")
     elsif errors.length > 0
@@ -82,6 +84,6 @@ class Checksum < Sensu::Plugin::Check::CLI
     else
       ok 'Files match.'
     end
-    
+
   end
 end
